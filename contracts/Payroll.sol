6f90e8bb61e9c4e9cb099da8ebc10040c81ae0b2
zhuolinho <zhuolinho@hotmail.com>
2017-11-17 23:48:55 +0800

    antd

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -10,22 +10,16 @@ contract Payroll is Ownable {
         uint salary;
         uint lastPayday;
     }
-    
+
     uint constant payDuration = 10 seconds;
-    
-    uint totalSalary = 0;
-    address owner;
+
+    uint totalSalary;
+    uint totalEmployee;
     mapping(address => Employee) public employees;
-    
+
     modifier employeeExist(address employeeId) {
         var employee = employees[employeeId];
         assert(employee.id != 0x0);
-        _;
-    }
-    
-    modifier employeeNotExist(address employeeId) {
-        var employee = employees[employeeId];
-        assert(employee.id == 0x0);
         _;
     }
 
@@ -35,57 +29,63 @@ contract Payroll is Ownable {
             .div(payDuration);
         employee.id.transfer(payment);
     }
-    
-    function addEmployee(address employeeId, uint salary) onlyOwner employeeNotExist(employeeId) {
+
+    function addEmployee(address employeeId, uint salary) onlyOwner {
+        var employee = employees[employeeId];
+        assert(employee.id == 0x0);
+
         employees[employeeId] = Employee(employeeId, salary.mul(1 ether), now);
         totalSalary = totalSalary.add(employees[employeeId].salary);
+        totalEmployee = totalEmployee.add(1);
     }
-    
+
     function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId) {
         var employee = employees[employeeId];
-        
+
         _partialPaid(employee);
         totalSalary = totalSalary.sub(employees[employeeId].salary);
         delete employees[employeeId];
-        
+        totalEmployee = totalEmployee.sub(1);
     }
-    
+
     function updateEmployee(address employeeId, uint salary) onlyOwner employeeExist(employeeId) {
         var employee = employees[employeeId];
-        
+
         _partialPaid(employee);
-        
         totalSalary = totalSalary.sub(employees[employeeId].salary);
         employees[employeeId].salary = salary.mul(1 ether);
         employees[employeeId].lastPayday = now;
         totalSalary = totalSalary.add(employees[employeeId].salary);
     }
-    
+
     function addFund() payable returns (uint) {
         return this.balance;
     }
-    
+
     function calculateRunway() returns (uint) {
         return this.balance.div(totalSalary);
     }
-    
+
     function hasEnoughFund() returns (bool) {
         return calculateRunway() > 0;
     }
-    
-    function getPaid() employeeExist(msg.sender)  {
+
+    function getPaid() employeeExist(msg.sender) {
         var employee = employees[msg.sender];
-        
+
         uint nextPayDay = employee.lastPayday.add(payDuration);
         assert(nextPayDay < now);
-        
+
         employees[msg.sender].lastPayday = nextPayDay;
         employee.id.transfer(employee.salary);
     }
-    
-    function changePaymentAddress(address newId) employeeExist(msg.sender) employeeNotExist(newId) {
-        employees[newId] = employees[msg.sender];
-        employees[newId].id = newId;
-        delete employees[msg.sender];
+
+    function checkInfo() returns (uint balance, uint runway, uint employeeCount) {
+        balance = this.balance;
+        employeeCount = totalEmployee;
+
+        if (totalSalary > 0) {
+            runway = calculateRunway();
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import React, {Component} from 'react'
+import PayrollContract from '../build/contracts/Payroll.json'
 import getWeb3 from './utils/getWeb3'
+
+import Accounts from './components/Accounts';
+import Employer from './components/Employer';
+import Employee from './components/Employee';
+import Common from './components/Common';
 
 import './css/oswald.css'
 import './css/open-sans.css'
@@ -8,87 +13,100 @@ import './css/pure-min.css'
 import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      storageValue: 0,
-      web3: null
+        this.state = {
+            storageValue: 0,
+            web3: null
+        }
     }
-  }
 
-  componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+    componentWillMount() {
+        // Get network provider and web3 instance.
+        // See utils/getWeb3 for more info.
 
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
+        getWeb3
+            .then(results => {
+                this.setState({
+                    web3: results.web3
+                })
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
-  }
+                // Instantiate contract once web3 provided.
+                this.instantiateContract()
+            })
+            .catch(() => {
+                console.log('Error finding web3.')
+            })
+    }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
+    instantiateContract() {
+        /*
+         * SMART CONTRACT EXAMPLE
+         *
+         * Normally these functions would be called in the context of a
+         * state management library, but for convenience I've placed them here.
+         */
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+        const contract = require('truffle-contract')
+        const Payroll = contract(PayrollContract)
+        Payroll.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+        // Declaring this for later so we can chain functions on SimpleStorage.
+        var PayrollInstance
 
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+        // Get accounts.
+        this.state.web3.eth.getAccounts((error, accounts) => {
+            this.setState({
+                accounts,
+                selectedAccount: accounts[0]
+            });
+            Payroll.deployed().then((instance) => {
+                PayrollInstance = instance
+                this.setState({
+                    payroll: instance
+                });
+            })
+        })
+    }
 
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
-  }
+    onSelectAccount = (ev) => {
+        this.setState({
+            selectedAccount: ev.target.text
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+    render() {
+        const {selectedAccount, accounts, payroll, web3} = this.state;
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+        if (!accounts) {
+            return <div>Loading</div>;
+        }
+
+        return (
+            <div className="App">
+                <nav className="navbar pure-menu pure-menu-horizontal">
+                    <a href="#" className="pure-menu-heading pure-menu-link">Payroll</a>
+                </nav>
+
+                <main className="container">
+                    <div className="pure-g">
+                        <div className="pure-u-1-3">
+                            <Accounts accounts={accounts} onSelectAccount={this.onSelectAccount}/>
+                        </div>
+                        <div className="pure-u-2-3">
+                            {
+                                selectedAccount === accounts[0] ?
+                                    <Employer employer={selectedAccount} payroll={payroll} web3={web3}/> :
+                                    <Employee employee={selectedAccount} payroll={payroll} web3={web3}/>
+                            }
+                            {payroll && <Common account={selectedAccount} payroll={payroll} web3={web3}/>}
+                        </div>
+                    </div>
+                </main>
             </div>
-          </div>
-        </main>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App
